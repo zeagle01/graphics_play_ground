e7f48d89105402c4233622a55b30943a774b97c5
zeagle <zeagle01@163.com>
2019-10-13 16:34:12 +0800

    specify log dir

--- examples/main.cpp
+++ examples/main.cpp
@@ -5,9 +5,19 @@
 
 
 #include "Scene.h"
+#include "gflags/gflags.h"
+#include "glog/logging.h"
+#include "boost/filesystem.hpp"
 
 
 int main(int argc,char** argv){
+
+	boost::filesystem::create_directory("log");
+	FLAGS_log_dir = "./log/";
+
+	gflags::ParseCommandLineFlags(&argc, &argv, true);
+	google::InitGoogleLogging(argv[0]);
+
 
     Scene s;
     s.main_loop(argc,argv);
--- shellet/test/test_main.cpp
+++ shellet/test/test_main.cpp
@@ -2,9 +2,21 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "glog/logging.h"
+#include "gflags/gflags.h"
+#include "boost/filesystem.hpp"
+
 int main(int argc,char** argv){
 
+
+	boost::filesystem::create_directory("log");
+	FLAGS_log_dir = "./log/";
+
+	gflags::ParseCommandLineFlags(&argc, &argv, true);
+	google::InitGoogleLogging(argv[0]);
+
     ::testing::InitGoogleMock(&argc,argv);
+
 
     //::testing::InitGoogleTest(&argc,argv);
     //::testing::InitGoogleMock(&argc,argv);
