88eda3cecc01f74c44592446ca8b8413b9e6e47b
zeagle <zeagle01@163.com>
2019-11-19 18:53:24 +0800

    big triangle

--- pyQuickImp/denaunay_tri.py
+++ pyQuickImp/denaunay_tri.py
@@ -3,9 +3,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x=np.linspace(0,np.pi,100);
-y=np.linspace(0,np.pi,100);
 
-plt.plot(x,y)
+
+points=np.random.rand(2,100)
+
+M=np.max(points)
+
+triangles=np.array([
+    [
+        [3*M,0],
+        [0, 3*M],
+        [-3 * M, -3 * M],
+    ]
+])
+
+
+#for p in points:
+
+
+
+
+fig=plt.figure()
+ax0=fig.add_subplot(221)
+ax0.scatter(points[0],points[1])
+
+ax1=fig.add_subplot(222)
+for t in triangles:
+    t_points=np.array([t[0],t[1],t[2],t[0]])
+    ax0.plot(t_points[:,0],t_points[:,1])
+    ax1.plot(t_points[:,0],t_points[:,1])
+
 plt.show()
 
