2522f3d635c915bd93f3fbd8fa8036d2e6937822
zeagle <zeagle01@163.com>
2019-11-20 19:02:59 +0800

    concatenate

--- pyQuickImp/denaunay_tri.py
+++ pyQuickImp/denaunay_tri.py
@@ -1,11 +1,12 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.linalg import solve
 
 
 
 
-points=np.random.rand(2,100)
+points=np.random.rand(2,2)
 
 M=np.max(points)
 
@@ -18,7 +19,45 @@ triangles=np.array([
 ])
 
 
-#for p in points:
+def point_in_triange(p,t):
+    d=np.array([
+        t[1]-t[0],
+        t[2] - t[0]
+    ]
+    )
+    dp=np.array(
+        p-t[0]
+    )
+    w=solve(d.transpose(),dp.transpose())
+    #p1=d.transpose().dot(w)+t[0]
+    w=np.array([w[0],w[1],1-w[0]-w[1]])
+    compare=np.logical_and(w < 1 ,w > 0)
+    if(compare.all()):
+        return True
+    else:
+        return False
+
+
+
+def find_triangle_point_reside(p,triangles):
+    for t in triangles:
+        if point_in_triange(p,t):
+            return t
+    return None
+
+
+for i,p in enumerate(points):
+    if i==0:
+        t=find_triangle_point_reside(p,triangles[0:])
+    else:
+        t = find_triangle_point_reside(p, triangles[1:])
+    if(t is not None):
+        nt0=np.array([[p, t[0], t[1]]])
+        nt1=np.array([[p, t[1], t[2]]])
+        nt2=np.array([[p, t[2], t[0]]])
+        triangles=np.concatenate((triangles,nt0))
+        triangles=np.concatenate((triangles,nt1))
+        triangles=np.concatenate((triangles,nt2))
 
 
 
