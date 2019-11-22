984e0fd603f715a2c080206947d73d5ab8b5b4db
zeagle <zeagle01@163.com>
2019-11-23 01:36:25 +0800

    grid points

--- pyQuickImp/denaunay_tri.py
+++ pyQuickImp/denaunay_tri.py
@@ -6,7 +6,16 @@ from scipy.linalg import solve
 
 
 
-points=np.random.rand(2,2)
+#points=np.random.rand(2,2)
+
+n=[3,3]
+x=np.linspace(0,1,n[0])
+y=np.linspace(0,1,n[1])
+x,y=np.meshgrid(x,y)
+x=x.reshape(n[0]*n[1])
+y=y.reshape(n[0]*n[1])
+points=np.array([x,y])
+points=points.transpose()
 
 M=np.max(points)
 
@@ -45,16 +54,37 @@ def find_triangle_point_reside(p,triangles):
             return t
     return None
 
+fig=plt.figure()
+ax0=fig.add_subplot(221)
+#ax0.scatter(points[0],points[1])
+
+ax1=fig.add_subplot(222)
+#for t in triangles:
+#    t_points=np.array([t[0],t[1],t[2],t[0]])
+#    ax0.plot(t_points[:,0],t_points[:,1])
+#    ax1.plot(t_points[:,0],t_points[:,1])
+
+
+def draw_triangle(ax,t):
+    t_points=np.array([t[0],t[1],t[2],t[0]])
+    ax.plot(t_points[:,0],t_points[:,1])
+
 
 for i,p in enumerate(points):
+
+    ax0.scatter(p[0],p[1])
     if i==0:
         t=find_triangle_point_reside(p,triangles[0:])
+        draw_triangle(ax0, t)
     else:
         t = find_triangle_point_reside(p, triangles[1:])
     if(t is not None):
         nt0=np.array([[p, t[0], t[1]]])
+        draw_triangle(ax0, nt0[0])
         nt1=np.array([[p, t[1], t[2]]])
+        draw_triangle(ax0, nt1[0])
         nt2=np.array([[p, t[2], t[0]]])
+        draw_triangle(ax0, nt2[0])
         triangles=np.concatenate((triangles,nt0))
         triangles=np.concatenate((triangles,nt1))
         triangles=np.concatenate((triangles,nt2))
@@ -62,15 +92,6 @@ for i,p in enumerate(points):
 
 
 
-fig=plt.figure()
-ax0=fig.add_subplot(221)
-ax0.scatter(points[0],points[1])
 
-ax1=fig.add_subplot(222)
-for t in triangles:
-    t_points=np.array([t[0],t[1],t[2],t[0]])
-    ax0.plot(t_points[:,0],t_points[:,1])
-    ax1.plot(t_points[:,0],t_points[:,1])
 
 plt.show()
-
