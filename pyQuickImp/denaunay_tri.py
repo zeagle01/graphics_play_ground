1b7bc9df94a808a1238d416c488e61ca2377987d
zeagle <zeagle01@163.com>
2019-11-28 21:58:10 +0800

    make plot nicer, hightlight current

--- pyQuickImp/denaunay_tri.py
+++ pyQuickImp/denaunay_tri.py
@@ -6,7 +6,7 @@ from scipy.linalg import solve
 
 
 display_delay=0.005
-points=np.random.rand(5,2)
+points=np.random.rand(16,2)
 
 #n=[2,2]
 #x=np.linspace(0,1,n[0])
@@ -26,6 +26,15 @@ triangles=np.array([
         [-3 * M, -3 * M],
     ]
 ])
+triangles_indices=np.array([
+    [
+        0,
+        1,
+        2
+    ]
+])
+
+
 
 
 def point_in_triange(p,t):
@@ -55,45 +64,42 @@ def find_triangle_point_reside(p,triangles):
             return ti,t
     return (-1,None)
 
-fig=plt.figure()
-ax0=fig.add_subplot(221)
-#ax0.scatter(points[0],points[1])
 
-ax1=fig.add_subplot(222)
-#for t in triangles:
-#    t_points=np.array([t[0],t[1],t[2],t[0]])
-#    ax0.plot(t_points[:,0],t_points[:,1])
-#    ax1.plot(t_points[:,0],t_points[:,1])
+fig1,ax=plt.subplots(2,2)
+ax=np.array(ax)
+ax=ax.reshape(-1)
 
 
 def draw_triangle(ax,t):
     t_points=np.array([t[0],t[1],t[2],t[0]])
-    ax.plot(t_points[:,0],t_points[:,1])
+    ax.plot(t_points[:,0],t_points[:,1],'black')
     plt.pause(display_delay)
 
 def draw_triangles(ax,triangles):
     for t in triangles:
         t_points=np.array([t[0],t[1],t[2],t[0]])
-        ax.plot(t_points[:,0],t_points[:,1])
+        ax.plot(t_points[:,0],t_points[:,1],'grey')
     plt.pause(display_delay)
 
 for i,p in enumerate(points):
 
     #plt.clf()
-    ax0.clear()
-    ax1.clear()
-    ax0.scatter(p[0],p[1])
+    ax[0].clear()
+    ax[1].clear()
+    ax[0].scatter(p[0],p[1])
     ti,t=find_triangle_point_reside(p,triangles)
-    draw_triangle(ax0,t)
-    draw_triangles(ax1,triangles)
+    draw_triangle(ax[0],t)
+    draw_triangles(ax[1],triangles)
+    draw_triangle(ax[1],t)
+    ax[1].scatter(points[0:i+1,0], points[0:i+1,1])
 
     if(t is not None):
         nt0=np.array([[p, t[0], t[1]]])
-        draw_triangle(ax0, nt0[0])
+        draw_triangle(ax[0], nt0[0])
         nt1=np.array([[p, t[1], t[2]]])
-        draw_triangle(ax0, nt1[0])
+        draw_triangle(ax[0], nt1[0])
         nt2=np.array([[p, t[2], t[0]]])
-        draw_triangle(ax0, nt2[0])
+        draw_triangle(ax[0], nt2[0])
         triangles=np.concatenate((triangles,nt0))
         triangles=np.concatenate((triangles,nt1))
         triangles=np.concatenate((triangles,nt2))
