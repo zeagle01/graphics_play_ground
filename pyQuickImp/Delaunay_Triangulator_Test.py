7bd6a556e09ced01aa99eca23412b740860a7028
zeagle <zeagle01@163.com>
2019-12-17 00:24:22 +0800

    Add_Two test

--- pyQuickImp/Delaunay_Triangulator_Test.py
+++ pyQuickImp/Delaunay_Triangulator_Test.py
@@ -8,7 +8,7 @@ from container_assert_helpers import *
 
 
 
-class Delaunay_Triangulator_Test(unittest.TestCase):
+class Delaunay_Triangulator_Add_One_Test(unittest.TestCase):
 
     def setUp(self):
         self.trianglutor=Delaunay_Triangulator()
@@ -19,12 +19,27 @@ class Delaunay_Triangulator_Test(unittest.TestCase):
         self.trianglutor.add_one()
 
 
-    def test_triangulate_three(self):
-        assertArrayEquals(self, self.trianglutor.get_triangles(), [[0,1,2]])
 
     def test_exsit_triangle(self):
         assertArrayEqualsUnordedly(self,self.trianglutor.get_exist_triangles(),[[0,1,3],[1,2,3],[0,2,3]])
 
+
+class Delaunay_Triangulator_Add_Two_Test(unittest.TestCase):
+
+    def setUp(self):
+        self.trianglutor=Delaunay_Triangulator()
+        np.random.seed(0)
+        #points = np.random.rand(3, 2)
+        points=np.array([[0,0],[0,1],[1,1]])
+        self.trianglutor.init(points)
+        self.trianglutor.add_one()
+        self.trianglutor.add_one()
+
+
+
+    def test_exsit_triangle(self):
+        #assertArrayEqualsUnordedly(self,self.trianglutor.get_exist_triangles(),[[0,1,3],[1,2,3],[0,2,3]])
+        pass
 
 
 if __name__ == '__main__':
