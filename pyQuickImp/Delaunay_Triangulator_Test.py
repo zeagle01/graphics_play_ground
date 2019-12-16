87200ecadcd4d9400c0f94aff8517db6b15378cc
zeagle <zeagle01@163.com>
2019-12-17 00:20:38 +0800

    add one vertex works

--- pyQuickImp/Delaunay_Triangulator_Test.py
+++ pyQuickImp/Delaunay_Triangulator_Test.py
@@ -13,12 +13,18 @@ class Delaunay_Triangulator_Test(unittest.TestCase):
     def setUp(self):
         self.trianglutor=Delaunay_Triangulator()
         np.random.seed(0)
-        points = np.random.rand(3, 2)
-        self.trianglutor.triangulate(points)
+        #points = np.random.rand(3, 2)
+        points=np.array([[0,0],[0,1],[1,1]])
+        self.trianglutor.init(points)
+        self.trianglutor.add_one()
 
 
     def test_triangulate_three(self):
         assertArrayEquals(self, self.trianglutor.get_triangles(), [[0,1,2]])
+
+    def test_exsit_triangle(self):
+        assertArrayEqualsUnordedly(self,self.trianglutor.get_exist_triangles(),[[0,1,3],[1,2,3],[0,2,3]])
+
 
 
 if __name__ == '__main__':
