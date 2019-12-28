77e1cdbade8c991f56970803ce74c31a8b2697ad
zeagle <zeagle01@163.com>
2019-12-29 03:54:03 +0800

    gradient descent and conjugate gradient

--- pyQuickImp/solve_equations.py
+++ pyQuickImp/solve_equations.py
@@ -3,34 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-
-A=np.array(
-   [
-       [4,-1,1],
-       [4, -8, 1],
-       [-2, 1, 5],
-   ]
-)
-
-b=np.array(
-    [7,-21,15]
-)
-
-x=np.linalg.solve(A,b)
-
-lambd,Evtor=np.linalg.eig(A)
-
-
-
-
-
-
 def Solving_Loop(method,A,b,x_0,iterations,err0):
 
     X = np.zeros((iterations + 1, len(x_0)))
     err = np.zeros(iterations+1)
     X[0] = x_0
-    method.precompute(A,b)
+    method.precompute(A,b,x_0)
     for i in range(iterations):
         X[i+1,:]=method.update(A,b,X[i,:])
         err[i]=np.linalg.norm(X[i+1,:]-X[i,:])
@@ -42,7 +20,7 @@ def Solving_Loop(method,A,b,x_0,iterations,err0):
 
 
 class Jacobi:
-    def precompute(self,A,b):
+    def precompute(self,A,b,x_0):
         self.D = np.diag(np.diag(A))
 
     def update(self,A,b,x):
@@ -50,7 +28,7 @@ class Jacobi:
 
 
 class Gauss_Seidel:
-    def precompute(self,A,b):
+    def precompute(self,A,b,x_0):
         self.L = np.tril(A)
         self.U = A - self.L
 
@@ -59,27 +37,69 @@ class Gauss_Seidel:
 
 
 class Gradient_Descent:
-    def precompute(self,A,b):
+    def precompute(self,A,b,x_0):
         pass
 
     def update(self,A,b,x):
-        return x-0.01*(b-np.dot(A,x))
+        r=b-np.dot(A,x)
+        alpha=np.dot(r,r)/np.dot(r,np.dot(A,r))
+        return x+alpha*r
 
-x_0=np.array([1,2,2])
-max_iteration=100
-limit_err=1e-12
-X,err=Solving_Loop(Jacobi(),A,b,x_0,max_iteration,limit_err)
-X1,err1=Solving_Loop(Gauss_Seidel(),A,b,x_0,max_iteration,limit_err)
-#X1,err1=Solving_Loop(Gradient_Descent(),A,b,x_0,max_iteration,limit_err)
+class Conjugate_Gradient:
+    def precompute(self,A,b,x_0):
+        self.r=b-np.dot(A,x_0)
+        self.p=self.r
+
+    def update(self,A,b,x):
+        alpha=np.dot(self.r,self.r)/np.dot(self.p,np.dot(A,self.p))
+        x=x+alpha*self.p
+        r0=np.copy(self.r)
+        self.r=self.r-alpha*np.dot(A,self.p)
+        beta=np.dot(self.r,self.r)/np.dot(r0,r0)
+        self.p=self.r+beta*self.p
+        return x
+
+
+A=np.array(
+   [
+#       [4,-1,1],
+#       [4, -8, 1],
+#       [-2, 1, 5],
+       [5,2],
+       [2, 3]
+   ]
+)
+
+b=np.array(
+#    [7,-21,15]
+     [1,1]
+)
+
+#x_0=np.array([1,2,2])
+x_0=np.array([0,0])
+
+x=np.linalg.solve(A,b)
 
 fig,ax=plt.subplots(1,2)
-ax[0].plot(X[:,0],X[:,1],'-o')
-ax[0].plot(X1[:,0],X1[:,1],'-o')
-#ax[0].plot(X,'-o')
-#ax[0].plot(X1,'-o')
 ax[1].set_yscale('log')
-ax[1].plot(err,'-o')
-ax[1].plot(err1,'-o')
+
+lambd,Evtor=np.linalg.eig(A)
+
+
+max_iteration=1000
+limit_err=1e-12
+
+methods=[Jacobi(),Gauss_Seidel(),Gradient_Descent(),Conjugate_Gradient()]
+names=['Jacobi','GS','GD','CG']
+R=[]
+for i in range(len(methods)):
+    R.append(Solving_Loop(methods[i],A,b,x_0,max_iteration,limit_err))
+    ax[0].plot(R[i][0][:,0],R[i][0][:,1],'-o',label=names[i])
+    ax[1].plot(R[i][1],'-o',label=names[i])
+
+ax[0].legend()
+ax[1].legend()
+
 
 plt.show()
 
