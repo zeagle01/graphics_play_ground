7947723683675bbdb812deb90b8453f7650dffb0
zeagle <zeagle01@163.com>
2020-05-04 23:13:50 +0800

    set width and height,clear every frame

--- examples/soft_render/main.cpp
+++ examples/soft_render/main.cpp
@@ -1,19 +1,44 @@
 
 #include <windows.h>
+
+#include <iostream>
+
+
+void line(int x0, int y0, int x1, int y1, COLORREF color,HDC dc)
+{
+	for (float t = 0.f; t < 1.f; t += 0.01f)
+	{
+		int x = x0 + (x1 - x0) * t;
+		int y = y0 + (y1 - y0) * t;
+		SetPixel(dc, x, y, color);
+	}
+}
+
 int main()
 {
 	HWND my_console = GetConsoleWindow();
 	HDC my_dc = GetDC(my_console);
-	for (int i = 0; i < 100; i++)
-	{
 
-		for (int j = 0; j < 1000; j++)
-		{
-			SetPixel(my_dc, i, j, RGB(255, 0, 0));
-		}
+	const int width = 800;
+	const int height = 600;
+	RECT r;
+	GetWindowRect(my_console, &r);
+	MoveWindow(my_console, r.left, r.top, width, height, TRUE);
+
+
+	float t = 0.f;
+	while (true)
+	{
+		system("cls");
+
+		line(300 + t, 300 , 200, 200, RGB(255, 100, 100), my_dc);
+		t += 1.0;
 	}
 
 	ReleaseDC(my_console, my_dc);
+	int a;
+	std::cin >> a;
+
 	return 0;
 }
 
