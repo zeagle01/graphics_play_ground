3794a72de5cd68294da4751dab3d3a1f6075c484
zeagle <zeagle01@163.com>
2020-05-05 21:02:28 +0800

    fill triangle in progress

--- examples/soft_render/main.cpp
+++ examples/soft_render/main.cpp
@@ -40,6 +40,49 @@ void line(int x0, int y0, int x1, int y1, COLORREF color,HDC dc)
 	}
 }
 
+struct vec2i
+{
+	int x;
+	int y;
+	vec2i operator+(const vec2i& other)
+	{
+		vec2i ret;
+		ret.x = x + other.x;
+		ret.y = y + other.y;
+		return ret;
+	}
+
+	vec2i operator-(const vec2i& other)
+	{
+		return { x + other.x,y + other.y };
+	}
+
+	vec2i operator*(float scalar)
+	{
+		return { int(scalar * x),int(scalar * y) };
+	}
+};
+
+void fill_triangle(vec2i v0, vec2i v1, vec2i v2, COLORREF color, HDC dc)
+{
+	if (v0.y > v1.y) std::swap(v0, v1);
+	if (v0.y > v2.y) std::swap(v0, v2);
+	if (v1.y > v2.y) std::swap(v1, v2);
+	//int total_height
+	for (int y = v0.y; y <= v1.y; y++)
+	{
+		float t1 = float(y - v0.y) / float(v1.y - v0.y);
+		float t2 = float(y - v0.y) / float(v2.y - v0.y);
+		vec2i A = v0 + (v1 - v0) * t1;
+		vec2i B = v1 + (v2 - v0) * t2;
+		line(A.x, A.y, B.x, B.y, color, dc);
+	}
+
+}
+
+
+
+
 int main()
 {
 	HWND my_console = GetConsoleWindow();
@@ -52,8 +95,9 @@ int main()
 	MoveWindow(my_console, r.left, r.top, width, height, TRUE);
 
 	Mesh_Loader mesh_loader;
-    mesh_loader.load_from_obj("cases/african_head.obj");
+	//mesh_loader.load_from_obj("cases/african_head.obj");
     //mesh_loader.load_from_obj("cases/flag.obj");
+    mesh_loader.load_from_obj("cases/1_triangle.obj");
 	auto positions = mesh_loader.get_positions();
 	auto indices = mesh_loader.get_indices();
 
@@ -64,6 +108,7 @@ int main()
 
 		for (size_t ti = 0; ti < indices.size()/3; ti++)
 		{
+			std::vector<vec2i> tv;
 			for (int vi = 0; vi < 3; vi++)
 			{
 				int vi_next = (vi + 1) % 3;
@@ -76,8 +121,9 @@ int main()
 				int y1 = (1.f - positions[v_next * 3 + 1]) * height * 0.5;
 
 				line(x0, y0, x1, y1, RGB(255, 100, 0), my_dc);
+				tv.push_back({ x0,y0 });
 			}
-
+			fill_triangle(tv[0], tv[1], tv[2], RGB(100, 255, 0), my_dc);
 		}
 
 		t += .01;
