5c832053d4f169896c3661d95d4079d5e9d61682
zeagle <zeagle01@163.com>
2020-05-06 18:48:01 +0800

    configure buffer

--- examples/gl_render/main.cpp
+++ examples/gl_render/main.cpp
@@ -1,9 +1,11 @@
 
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
 #include "glog/logging.h"
 #include "gflags/gflags.h"
 #include <filesystem>
+
 
 int main(int argc, char** argv)
 {
@@ -15,7 +17,17 @@ int main(int argc, char** argv)
 	gflags::ParseCommandLineFlags(&argc, &argv, true);
 	google::InitGoogleLogging(argv[0]);
 
-	glfwInit();
+
+	auto status = glfwInit();
+	if (status == GLFW_TRUE)
+	{
+		LOG(INFO) << "glfw init succeed! ";
+	}
+	else
+	{
+		LOG(ERROR) << "glfw init failed! ";
+	}
+
 
 	GLFWwindow* window;
 	window = glfwCreateWindow(800, 600, "gl window", nullptr, nullptr);
@@ -29,11 +41,57 @@ int main(int argc, char** argv)
 		LOG(ERROR) << "window create failed!" ;
 	}
 
+	glfwMakeContextCurrent(window);
+	LOG(INFO) << "gl context " ;
+
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+		LOG(ERROR)<<"glad load failed";
+	}
+	else {
+		LOG(INFO)<<"glad load succeed!";
+	}
+
+	LOG(INFO) << "gl version " << glGetString(GL_VERSION);
+
+
+	std::vector<float> position
+	{
+		-0.5f,-0.5f,
+		0.0f,0.5f,
+		0.5,-0.5f
+	};
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+
+	glBindBuffer(GL_ARRAY_BUFFER,buffer);
+
+	int position_attrib = 0;
+	glBufferData(GL_ARRAY_BUFFER, position.size() * sizeof(float),position.data(),GL_DYNAMIC_DRAW);
+
+	glEnableVertexAttribArray(position_attrib);
+
+	glVertexAttribPointer(position_attrib, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), (void*)0);
+
+
+	glBindBuffer(GL_ARRAY_BUFFER,0);
+
 	while (!glfwWindowShouldClose(window))
 	{
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindBuffer(GL_ARRAY_BUFFER, buffer);
+		glDrawArrays(GL_TRIANGLES, 0, position.size() / 2);
+		//glDrawElements(GL_TRIANGLES,3,)
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	glfwTerminate();
 
 	return 0;
 
