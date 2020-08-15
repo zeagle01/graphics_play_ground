e47afe75b4755d8cf7c028b5037c69bf4269ab3f
zeagle <zeagle01@163.com>
2020-08-15 15:35:09 +0800

    set up soft render 2 project

--- shellet/shellet/test/test_main.cpp
+++ shellet/shellet/test/test_main.cpp
@@ -1,17 +1,16 @@
 
 
 #include "gmock/gmock.h"
-#include "gtest/gtest.h"
 #include "glog/logging.h"
 #include "gflags/gflags.h"
-#include "boost/filesystem.hpp"
+#include <filesystem>
 
 int main(int argc,char** argv){
 
 
 	//std::string currentPath = boost::filesystem::system_complete(".").string();
 
-	boost::filesystem::create_directory("log");
+	std::filesystem::create_directory("log");
 	FLAGS_log_dir = "./log/";
 
 	gflags::ParseCommandLineFlags(&argc, &argv, true);
