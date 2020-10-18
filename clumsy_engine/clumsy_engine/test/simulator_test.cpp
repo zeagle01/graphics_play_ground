b344aa0eaa2288cf39a17129161915eba285383a
zeagle <zeagle01@163.com>
2020-10-18 12:37:10 +0800

    test refactor one triangle -> one vertex

--- clumsy_engine/clumsy_engine/test/simulator_test.cpp
+++ clumsy_engine/clumsy_engine/test/simulator_test.cpp
@@ -11,19 +11,35 @@ using namespace testing;
 using namespace clumsy_engine;
 
 
-class One_Triangle :public Test
+class One_Vertex :public Test
 {
 public:
-	One_Triangle()
+	One_Vertex()
 	{
-		X = std::vector<float>(9, 0.f);
+		m_positions = std::vector<float>(3, 0.f);
 
-		m_sim.set_mesh(X, T);
+		m_vertex_num = m_positions.size() / 3;
+
+		m_sim.set_mesh(m_positions, m_triangles);
 	}
 protected:
 
-	std::vector<float> X;
-	std::vector<int> T;
+	std::vector<float>  setup_and_update()
+	{
+		m_sim.set_time_step(0.1f);
+
+		m_sim.update();
+
+		return m_sim.get_delta_positions();
+
+	}
+
+
+	std::vector<float> m_positions;
+
+	std::vector<int> m_triangles;
+
+	int m_vertex_num;
 
 	Simulator m_sim;
 
@@ -32,45 +48,30 @@ protected:
 
 
 
-TEST_F(One_Triangle, update_with_gravity_only)
+TEST_F(One_Vertex, update_with_gravity_only)
 {
+
 	float time_stpe = 0.1f;
-	std::vector<float> masses(3, 1.f);
+	std::vector<float> masses(m_vertex_num, 1.f);
 
-	int vertex_num = X.size()/3;
+	m_sim.add_interaction(std::make_unique<Gravity>(m_vertex_num, 0, -10.f, 0, time_stpe, masses));
 
-	m_sim.add_interaction(std::make_unique<Gravity>(vertex_num, 0, -10.f, 0, time_stpe, masses));
-
-	m_sim.set_time_step(0.1f);
-
-	m_sim.update();
-
-
-	auto act = m_sim.get_delta_positions();
+	auto act = setup_and_update();
 
 	std::vector<float> exp = {
-		0,-0.1,0,
-		0,-0.1,0,
 		0,-0.1,0
-
 	};
 
 	EXPECT_THAT(act, exp);
 }
 
-TEST_F(One_Triangle, without_any_interaction_just_floating_there)
+TEST_F(One_Vertex, without_any_interaction_just_floating_there)
 {
-	m_sim.set_time_step(0.1f);
 
-	m_sim.update();
-
-	auto act = m_sim.get_delta_positions();
+	auto act = setup_and_update();
 
 	std::vector<float> exp = {
-		0,0.0,0,
-		0,0.0,0,
 		0,0.0,0
-
 	};
 
 	EXPECT_THAT(act, exp);
