e4b11ed2f7560a09d5bfa74f5ab219f8d445867c
zeagle <zeagle01@163.com>
2021-03-14 00:13:55 +0800

    assign and access

--- common/matrix_math/include/matrix_math/matrix_math.h
+++ common/matrix_math/include/matrix_math/matrix_math.h
@@ -1,12 +1,34 @@
 
 
+#include <initializer_list>
 
 template<size_t Row,size_t Col,typename T>
-class mat
+class mat;
+
+template<size_t Row,typename T>
+using vec = mat<Row, 1, T>;
+
+template<size_t Row,size_t Col,typename T>
+class mat 
 {
 public:
 	mat(){}
+	mat(const std::initializer_list<std::initializer_list<T>> data_list)//col major
+	{
+		int ci = 0;
+		for ( auto it_col = data_list.begin(); it_col != data_list.end(); it_col++,ci++)
+		{
+			int ri = 0;
+			for ( auto it_row = it_col->begin(); it_row != it_col->end(); it_row++,ri++)
+			{
+				data[ci][ri]=*it_row;
+			}
+		}
 
+	}
+
+	vec<Row, T>& operator[](int c) { return col_vecs[c]; }
+	const vec<Row, T>& operator[](int c) const { return col_vecs[c]; }
 
 
 
@@ -14,5 +36,50 @@ private:
 	static constexpr size_t row = Row;
 	static constexpr size_t col = Col;
 private:
-	T data[row * col];
+	union {
+		T data[row][col];
+		T flat[row * col];
+		vec<Row, T> col_vecs[col];
+	};
 };
+
+template<size_t Row,typename T>
+class mat<Row, 1, T> 
+{
+public:
+	mat() {}
+	mat(const std::initializer_list<T> data_list)//col major
+	{
+		int ri = 0;
+		for ( auto it_row = data_list.begin(); it_row != data_list.end(); it_row++,ri++)
+		{
+			data[ri] = *it_row;
+		}
+
+	}
+	T& operator[](int i) { return data[i]; }
+	const T& operator[](int i) const { return data[i]; }
+private:
+	T data[Row];
+};
+
+
+
+
+
+
+//////////////////////type alias///////////////////////
+using vec2f = vec<2, float>;
+using vec3f = vec<3, float>;
+using vec4f = vec<4, float>;
+
+using vec2i = vec<2, int>;
+using vec3i = vec<3, int>;
+using vec4i = vec<4, int>;
+
+template<typename T>
+using mat4x4 = mat<4, 4, T>;
+
+using mat4x4f = mat<4, 4, float>;
+using mat3x3f = mat<3, 3, float>;
+using mat2x2f = mat<2, 2, float>;
--- unit_test/common/matrix_math_test/matrix_math_test.cpp
+++ unit_test/common/matrix_math_test/matrix_math_test.cpp
@@ -5,11 +5,53 @@
 #include "gmock/gmock.h"
 
 
+using namespace testing;
 
 
 
-TEST(Matrix_Math_Test, vec_access)
+TEST(Matrix_Test, acess_by_element)
 {
-	mat<1, 2,float> m;
+	mat<2, 2, float> m
+	{
+		{0,1},
+		{2,3}
+	};
 
+	EXPECT_THAT(m[0][0], Eq(0));
+	EXPECT_THAT(m[0][1], Eq(1));
+	EXPECT_THAT(m[1][0], Eq(2));
+	EXPECT_THAT(m[1][1], Eq(3));
+}
+
+TEST(Matrix_Test, assign_by_element)
+{
+	mat<2, 2, float> m
+	{
+		{0,1},
+		{2,3}
+	};
+	m[0][0] = 5;
+
+	EXPECT_THAT(m[0][0], Eq(5));
+}
+
+TEST(Matrix_Test, get_column)
+{
+	mat<2, 2, float> m
+	{
+		{0,1},
+		{2,3}
+	};
+	vec<2,float> c = m[0];
+
+	EXPECT_THAT(c[0], Eq(0));
+	EXPECT_THAT(c[1], Eq(1));
+}
+
+TEST(Vector_Test, get_element)
+{
+	vec<2,  float> v{0,1};
+
+	EXPECT_THAT(v[0], Eq(0));
+	EXPECT_THAT(v[1], Eq(1));
 }
