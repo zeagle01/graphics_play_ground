15a46851ac40606c1404772bfa5311ed83f34152
zeagle <zeagle01@163.com>
2021-03-14 13:37:51 +0800

    norm and is_near

--- common/matrix_math/include/matrix_math/matrix_math.h
+++ common/matrix_math/include/matrix_math/matrix_math.h
@@ -1,6 +1,8 @@
 
 
 #include <initializer_list>
+#include <numeric>
+#include <algorithm>
 
 template<size_t Row,size_t Col,typename T>
 class mat;
@@ -30,9 +32,12 @@ public:
 	vec<Row, T>& operator[](int c) { return col_vecs[c]; }
 	const vec<Row, T>& operator[](int c) const { return col_vecs[c]; }
 
+	T * get_flat(){ return flat; }
+	T const* get_flat()const { return flat; }
 
 
-private:
+
+public:
 	static constexpr size_t row = Row;
 	static constexpr size_t col = Col;
 private:
@@ -59,11 +64,177 @@ public:
 	}
 	T& operator[](int i) { return data[i]; }
 	const T& operator[](int i) const { return data[i]; }
+
+	T * get_flat(){ return data; }
+	T const* get_flat()const { return data; }
 private:
 	T data[Row];
 };
 
+////////////////////////////
+template<size_t Row,size_t Col,typename T>
+static mat<Row, Col, T> get_uniform(T value)
+{
+	mat<Row, Col, T> ret;
+	for (int i = 0; i < Row * Col; i++) 
+	{
+		ret.get_flat()[i] = value; 
+	}
+	return ret;
+}
 
+template<size_t row, size_t col, typename T>
+static inline mat<row, col, T> get_identity()
+{
+	mat<row, col, T> ret;
+	for (int ri = 0; ri < row; ri++)
+	{
+		for (int ci = 0; ci < col; ci++)
+		{
+			if (ri == ci)
+			{
+				ret[ci][ri] = 1.f;
+			}
+			else
+			{
+				ret[ci][ri] = 0.f;
+			}
+
+		}
+	}
+	return ret;
+}
+
+////////////////////////////
+
+
+///////////////////////
+
+template<typename T>
+struct Add
+{
+	T operator()(T v0, T v1)
+	{
+		return v0 + v1;
+	}
+};
+
+template<typename T>
+struct Substract
+{
+	T operator()(T v0, T v1)
+	{
+		return v0 - v1;
+	}
+};
+
+template<typename T>
+struct Multiply
+{
+	T operator()(T v0, T v1)
+	{
+		return v0 * v1;
+	}
+};
+
+template<typename T>
+struct Divide
+{
+	T operator()(T v0, T v1)
+	{
+		return v0 / v1;
+	}
+};
+
+
+template<size_t Row, size_t Col, typename T, typename F>
+static mat<Row, Col, T> matrix_elementwise_binary_op(const mat<Row, Col, T>& m0, const mat<Row, Col, T>& m1,F fn)
+{
+	mat<Row, Col, T> ret;
+	std::transform(m0.get_flat(), m0.get_flat() + Row * Col, m1.get_flat(), ret.get_flat(), fn);
+	return ret;
+}
+
+template<size_t Row, size_t Col, typename T, typename F>
+static mat<Row, Col, T> matrix_elementwise_unary_op(const mat<Row, Col, T>& m0, F fn)
+{
+	mat<Row, Col, T> ret;
+	std::transform(m0.get_flat(), m0.get_flat() + Row * Col, ret.get_flat(), fn);
+	return ret;
+}
+
+
+///////////////////////
+#define MATRIX_ELEMENTWISE_BINARY_OP(x,op) \
+template<size_t Row,size_t Col,typename T> \
+static mat<Row, Col, T> operator##x(const mat<Row, Col, T>& m0,const mat<Row, Col, T>& m1) \
+{ \
+	return matrix_elementwise_binary_op<Row,Col,T>(m0, m1,op<T>()); \
+} \
+
+//a0+a1
+MATRIX_ELEMENTWISE_BINARY_OP(+,Add)
+
+//a0-a1
+MATRIX_ELEMENTWISE_BINARY_OP(-,Substract)
+
+//a0*a1
+MATRIX_ELEMENTWISE_BINARY_OP(*,Multiply)
+
+//a0/a1
+MATRIX_ELEMENTWISE_BINARY_OP(/,Divide)
+///////////////////////
+
+
+
+/////////////////////
+template<int P >
+struct Vectorized_Norm
+{
+
+	template< size_t Row, size_t Col, typename T>
+	T operator()(const mat<Row, Col, T>& m)
+	{
+		T p = P;
+		auto power_fn = [p](T v)
+		{
+			return std::pow<T>(v, p);
+		};
+
+		auto powered = matrix_elementwise_unary_op<Row, Col, T >(m, power_fn);
+		T ret = std::accumulate(powered.get_flat(), powered.get_flat() + Row * Col, T(0));
+		ret = std::pow(ret, 1 / p);
+		return ret;
+	}
+};
+
+//infinity norm
+template<>
+struct Vectorized_Norm<0>
+{
+	template< size_t Row, size_t Col, typename T>
+	T operator()(const mat<Row, Col, T>& m)
+	{
+		auto max_value = [](T accumulated, T new_v)
+		{
+			T abs_new_v = std::abs(new_v);
+			T abs_accumulated = std::abs(accumulated);
+			return abs_new_v > abs_accumulated ? abs_new_v : abs_accumulated;
+		};
+
+		T ret = std::accumulate(m.get_flat(), m.get_flat() + Row * Col, T(0), max_value);
+		return ret;
+	}
+};
+
+
+template<int P, size_t Row, size_t Col, typename T  >
+static bool is_near(const mat<Row, Col, T>& m0, const mat<Row, Col, T>& m1, T threshold = 0)
+{
+	T norm = Vectorized_Norm<P>()(m0 - m1);
+	return norm <= threshold;
+}
+/////////////////////
 
 
 
--- unit_test/common/matrix_math_test/matrix_math_test.cpp
+++ unit_test/common/matrix_math_test/matrix_math_test.cpp
@@ -55,3 +55,92 @@ TEST(Vector_Test, get_element)
 	EXPECT_THAT(v[0], Eq(0));
 	EXPECT_THAT(v[1], Eq(1));
 }
+
+TEST(Matrix_Test, assign_construct)
+{
+	mat<2, 2, float> m
+	{
+		{0,1},
+		{2,3}
+	};
+	mat<2, 2, float> m1(m);
+
+	EXPECT_THAT(m1[0][0], Eq(0));
+	EXPECT_THAT(m1[0][1], Eq(1));
+	EXPECT_THAT(m1[1][0], Eq(2));
+	EXPECT_THAT(m1[1][1], Eq(3));
+}
+
+TEST(Vector_Test, assign_construct)
+{
+	vec2f v{0,1};
+	auto v1 = v;
+
+	EXPECT_THAT(v1[0], Eq(0));
+	EXPECT_THAT(v1[1], Eq(1));
+}
+
+
+
+
+/// add
+TEST(Matrix_Test, add)
+{
+	mat2x2f m0 = get_uniform<2, 2, float>(1);
+	mat2x2f m1 = get_uniform<2, 2, float>(1);
+
+	auto exp = m0 + m1;
+	EXPECT_THAT(exp[0][0], Eq(2));
+	EXPECT_THAT(exp[0][1], Eq(2));
+	EXPECT_THAT(exp[1][0], Eq(2));
+	EXPECT_THAT(exp[1][1], Eq(2));
+}
+
+TEST(Vector_Test, add)
+{
+	vec2f v0{1,1};
+	vec2f v1{1,1};
+
+	auto exp = v0 + v1;
+	EXPECT_THAT(exp[0], Eq(2));
+	EXPECT_THAT(exp[1], Eq(2));
+}
+
+/// norm
+TEST(Vector_Test, norm)
+{
+	vec2f v0{1,1};
+	vec2f v1{3,4};
+
+	float norm1 = Vectorized_Norm<1>()(v0); //1+1
+	float norm2 = Vectorized_Norm<2>()(v1); // sqrt(3*3+4*4)=5
+	float norm3 = Vectorized_Norm<0>()(v1); //infinity norm
+	float norm4 = Vectorized_Norm<1>()(v1 - v0); //2+3
+	EXPECT_THAT(norm1, Eq(2));
+	EXPECT_THAT(norm2, Eq(5));
+	EXPECT_THAT(norm3, Eq(4));
+	EXPECT_THAT(norm4, Eq(5));
+}
+
+TEST(Matrix_Test, norm)
+{
+	mat2x2f m0 = get_uniform<2, 2, float>(1);
+	float exp = Vectorized_Norm<1>()(m0);
+	EXPECT_THAT(exp, Eq(4));
+}
+
+
+/// is_near
+TEST(Vector_Test, is_near)
+{
+	vec2f v0{1,1};
+	vec2f v1{1,1};
+	vec2f v2{1,1.1};
+
+	auto exp1 = is_near<1>(v1, v0);
+	auto exp2 = is_near<1>(v2, v0);
+	auto exp3 = is_near<1>(v2, v0, 0.11f);
+	EXPECT_TRUE(exp1);
+	EXPECT_FALSE(exp2);
+	EXPECT_TRUE(exp3);
+}
