d0793d2cde364a95ea8f012ecf9c62029421b6f4
zeagle <zeagle01@163.com>
2021-09-04 11:46:09 +0800

    delete old

--- clumsy_engine/clumsy_engine/src/linear_solver_Jacobi.cpp
+++ clumsy_engine/clumsy_engine/src/linear_solver_Jacobi.cpp
@@ -42,56 +42,6 @@ namespace clumsy_engine
 		auto r = b - A * x0;
 		auto delta_x= diagonal_inverse(A, r);
 		return x0 + delta_x;
-
-
-
-		if (lhs.size() != vertexNum)
-		{
-			lhs.resize(vertexNum, get_uniform<3, 3, float>(0));
-			rhs.resize(vertexNum, get_uniform<3, 1, float>(0));
-		}
-
-		lhs.assign(vertexNum, get_uniform<3, 3, float>(0));
-		rhs.assign(vertexNum, get_uniform<3, 1, float>(0));
-
-
-		for (int i = 0; i < equations.size(); i++)
-		{
-			auto& equation = equations[i];
-
-			int element_vertex_num = equation.stencil.size();
-
-			for (int evi = 0; evi < element_vertex_num; evi++)
-			{
-				int ev_r = equation.stencil[evi];
-
-				lhs[ev_r] = lhs[ev_r] + equation.A[evi * element_vertex_num + evi];
-
-				rhs[ev_r] = rhs[ev_r] + equation.b[evi];
-
-				for (int evj = 0; evj < element_vertex_num; evj++)
-				{
-					if (evj == evi) {
-						continue;
-					}
-
-					int ev_c = equation.stencil[evj];
-
-					rhs[ev_r] = rhs[ev_r] - (equation.A[evi * element_vertex_num + evj] & x0[ev_c]);
-				}
-			}
-		}
-
-		for (int i = 0; i < vertexNum; i++)
-		{
-
-			if (i == 0)
-			{
-				continue; //fix point
-			}
-			ret[i] = rhs[i] | lhs[i];
-		}
-		return ret;
 	}
 
 }
