cb6dd63999d1809c0760fbebbcb96f3d2e49e034
zeagle <zeagle01@163.com>
2021-11-27 02:02:23 +0800

    matrix math v2

--- common/matrix_math/include/matrix_math/matrix_math.h
+++ common/matrix_math/include/matrix_math/matrix_math.h
@@ -469,6 +469,7 @@ struct determinant<1, T>
 	}
 };
 
+
 template<size_t N, typename T>
 constexpr inline T cofactor(const mat<N, N, T>& a, const int row, const int col)
 {
@@ -537,6 +538,270 @@ using mat3x2f = mat<3, 2, float>;
 using mat2x2f = mat<2, 2, float>;
 
 
+
+
+namespace matrix_math
+{
+
+#define MAT_DATA(Row,Col,T) \
+	T data[Row * Col]; \
+	const T& operator()(int ri, int ci) const { return data[ri + ci * Row]; }\
+	T& operator()(int ri, int ci) { return data[ri + ci * Row]; } \
+	const T* operator&() const {return data;} \
+	T* operator&(){ return data;} \
+
+#define VEC_ACCESS(T) \
+		const T& operator()(int i) const { return data[i]; } \
+		T& operator()(int i) { return data[i]; }  \
+
+
+	template<int Row, int Col, typename T>
+	struct mat  
+	{
+		MAT_DATA(Row,Col,T)
+	};
+
+	template<int Row,  typename T>
+	struct mat<Row,1,T>
+	{
+		MAT_DATA(Row, 1, T)
+		VEC_ACCESS(T)
+	};
+
+	template<int Col,  typename T>
+	struct mat<1,Col,T>
+	{
+		MAT_DATA(1, Col, T)
+		VEC_ACCESS(T)
+	};
+
+	template<typename T>
+	struct mat<1,1,T>
+	{
+		MAT_DATA(1, 1, T)
+		VEC_ACCESS(T)
+		operator T() const { return data[0]; }
+		operator T& () { return data[0]; }
+	};
+
+
+	template<typename T> struct Add { static T apply(T v0, T v1) { return v0 + v1; } };
+	template<typename T> struct Substract { static T apply(T v0, T v1) { return v0 - v1; } };
+	template<typename T> struct Multiply { static T apply(T v0, T v1) { return v0 * v1; } };
+	template<typename T> struct Divide { static T apply(T v0, T v1) { return v0 / v1; } };
+
+
+	template<typename T, int N, template<typename> typename Op>
+	static inline void BinaryOp(T* a, const T* b, const T* c)
+	{
+		for (int i = 0; i < N; i++)
+		{
+			a[i] = Op<T>::apply(b[i], c[i]);
+		}
+	}
+
+#define COMPONENTWISE_BINARY(name,component_op) \
+	template<int Row,int Col,typename T> \
+	static inline mat<Row, Col, T> name(mat<Row, Col, T>& a, mat<Row, Col, T>& b)\
+	{\
+		mat<Row, Col, T> ret;\
+		BinaryOp<T, Row* Col, component_op>(&ret, &a, &b);\
+		return ret;\
+	}\
+
+COMPONENTWISE_BINARY(operator+,Add)
+COMPONENTWISE_BINARY(operator-,Substract)
+
+	template<int Row, int N, int Col, typename T>
+	static inline mat<Row, Col, T> operator*(const mat<Row, N, T>& a, const mat<N, Col, T>& b)
+	{
+		mat<Row, Col, T> ret;
+		for (int ri = 0; ri < Row; ri++)
+		{
+			for (int ci = 0; ci < Col; ci++)
+			{
+				ret(ri, ci) = 0.f;
+				for (int ni = 0; ni < N; ni++)
+				{
+					ret(ri, ci) += a(ri, ni) * b(ni, ci);
+				}
+			}
+		}
+		return ret;
+	}
+
+	template<int Row,int Col, typename T>
+	static inline mat<Row, Col, T> operator*(const mat<Row, Col, T>& a, const T& s)
+	{
+		mat<Row, Col, T> ret;
+		for (int i = 0; i < Row * Col; i++)
+		{
+			(&ret)[i] = (&a)[i] * s;
+		}
+		return ret;
+	}
+
+	template<int Row,int Col, typename T>
+	static inline mat<Row, Col, T> operator*(const T& s, const mat<Row, Col, T>& a) { return a * s; }
+
+	template<int Row,int Col, typename T>
+	static inline mat<Row, Col, T> operator/(const mat<Row, Col, T>& a, const T& s)
+	{
+		mat<Row, Col, T> ret;
+		for (int i = 0; i < Row * Col; i++)
+		{
+			(&ret)[i] = (&a)[i] / s;
+		}
+		return ret;
+	}
+
+
+	//cross product
+	template< typename T>
+	static inline mat<3, 1, T> operator ^ (const mat<3, 1, T>& a, const mat<3, 1, T>& b)
+	{
+		return { a(1) * b(2) - a(2) * b(1), a(2) * b(0) - a(0) * b(2), a(0) * b(1) - a(1) * b(0) };
+	}
+
+	template<int Row,int Col, typename T>
+	static inline mat<Row, 1, T> get_column(const mat<Row, Col, T>& a, int column)
+	{
+		mat<Row, 1, T> ret;
+		for (int i = 0; i < Row; i++)
+		{
+			ret(i) = a(i, column);
+		}
+		return ret;
+	}
+
+	template<int Row,int Col, typename T>
+	static inline mat<Col, Row, T> transpose(const mat<Row, Col, T>& a)
+	{
+		mat<Col, Row, T> ret;
+		for (int ri = 0; ri < Row; ri++)
+		{
+			for (int ci = 0; ci < Col; ci++)
+			{
+				ret(ci, ri) = a(ri, ci);
+			}
+		}
+		return ret;
+	}
+
+
+	//inverse wise
+
+	template<int N, typename T>
+	static	T determinant(const mat<N, N, T>& a) {
+		if constexpr (N == 1)
+		{
+			return a;
+		}
+		else
+		{
+			T sum = 0;
+			for (int i = 0; i < N; i++)
+			{
+				sum += a(0, i) * cofactor(a, i, 0);
+			}
+			return sum;
+		}
+	}
+
+	template<int N, typename T>
+	static inline mat<N - 1, N - 1, T> get_minor(const mat<N, N, T>& a, const int r, const int c)
+	{
+		mat<N - 1, N - 1, T> ret;
+		for (int ri = 0; ri < N - 1; ri++)
+		{
+			for (int ci = 0; ci < N - 1; ci++)
+			{
+				int rr = ri < r ? ri : ri + 1;
+				int cc = ci < c ? ci : ci + 1;
+				ret(ci, ri) = a(cc, rr);
+			}
+		}
+		return ret;
+	}
+
+	template<int N, typename T>
+	constexpr inline T cofactor(const mat<N, N, T>& a, const int row, const int col)
+	{
+		T sign = (row + col) % 2 ? -1 : 1;
+		return  determinant<N - 1, T>(get_minor(a, row, col)) * sign;
+	}
+
+	template<int N, typename T>
+	inline mat<N, N, T> adjoint(const mat<N, N, T>& a) {
+		mat<N, N, T> ret;
+		for (int ri = 0; ri < N; ri++) {
+			for (int ci = 0; ci < N; ci++)
+			{
+				ret(ri, ci) = cofactor(a, ri, ci);
+			}
+		}
+		return ret;
+	}
+
+	template<int Row, int Col, typename T>
+	static inline mat<Row, Col, T> operator/(const mat<Row, Col, T>& m0, const mat<Row, Row, T>& m1) {
+
+		mat<Row, Row, T> adj = adjoint(m1);
+		T det = determinant<Row, T>(m1);
+		auto inv = adj / det;
+		return inv * m0;
+	}
+
+
+
+
+	//////////////////
+	template<size_t N, typename T>
+	static inline mat<N, N, T> get_identity()
+	{
+		mat<N, N, T> ret;
+		for (int ri = 0; ri < N; ri++)
+		{
+			for (int ci = 0; ci < N; ci++)
+			{
+				if (ri == ci)
+				{
+					ret(ri, ci) = 1.f;
+				}
+				else
+				{
+					ret(ri, ci) = 0.f;
+				}
+			}
+		}
+		return ret;
+	}
+
+	template<size_t Row, size_t Col, typename T>
+	static inline mat<Row, Col, T> get_uniform(T value)
+	{
+		mat<Row, Col, T> ret;
+		for (int i = 0; i < Row * Col; i++)
+		{
+			(&ret)[i] = value;
+		}
+		return ret;
+	}
+
+
+using vec4 = mat<4, 1, float>;
+using vec3 = mat<3, 1, float>;
+using vec2 = mat<2, 1, float>;
+
+using rvec4 = mat<1, 4, float>;
+using rvec3 = mat<1, 3, float>;
+using rvec2 = mat<1, 2, float>;
+
+using mat4 = mat<4, 4, float>;
+using mat3 = mat<3, 3, float>;
+using mat2 = mat<2, 2, float>;
+
+}
 
 
 
--- unit_test/common/matrix_math_test/matrix_math_test.cpp
+++ unit_test/common/matrix_math_test/matrix_math_test.cpp
@@ -271,3 +271,91 @@ TEST(Matrix_Test, normalized_vec)
 	EXPECT_THAT(act(0), Eq(exp(0)));
 	EXPECT_THAT(act(1), Eq(exp(1)));
 }
+
+
+namespace matrix_math
+{
+	TEST(Matrix_Math_Test, init)
+	{
+		mat< 2, 1, float> act{ 1,2 };
+
+		EXPECT_THAT(act(0, 0), Eq(1));
+		EXPECT_THAT(act(1, 0), Eq(2));
+
+		EXPECT_THAT(act(0), Eq(1));
+		EXPECT_THAT(act(1), Eq(2));
+	}
+
+	TEST(Matrix_Math_Test, add_minus)
+	{
+		mat< 2, 1, float> a{ 1,2 };
+		mat< 2, 1, float> b{ 1,2 };
+
+		auto act = a + b;
+
+		EXPECT_THAT(act(0), Eq(2));
+		EXPECT_THAT(act(1), Eq(4));
+
+		act = a - b;
+
+		EXPECT_THAT(act(0), Eq(0));
+		EXPECT_THAT(act(1), Eq(0));
+	}
+
+	TEST(Matrix_Math_Test, matrix_multiply_scalar)
+	{
+		mat< 2, 1, float> a{ 1,2 };
+
+		auto act = a * 2.f;
+
+		EXPECT_THAT(act(0), Eq(2));
+		EXPECT_THAT(act(1), Eq(4));
+
+		act = 3.f * a;
+
+		EXPECT_THAT(act(0), Eq(3));
+		EXPECT_THAT(act(1), Eq(6));
+	}
+
+	TEST(Matrix_Math_Test, matrix_multiply)
+	{
+		mat< 2, 1, float> a{ 1,2 };
+		mat< 1, 2, float> b{ 1,2 };
+
+		auto act = a * b;
+
+		EXPECT_THAT(act(0,0), Eq(1));
+		EXPECT_THAT(act(1,0), Eq(2));
+		EXPECT_THAT(act(0,1), Eq(2));
+		EXPECT_THAT(act(1,1), Eq(4));
+
+		auto act1 = b * a;
+
+		EXPECT_THAT(act1, Eq(5));
+	}
+
+	TEST(Matrix_Math_Test, cross)
+	{
+		vec3 v0{ 1,0,0 };
+		vec3 v1{ 0,1,0 };
+
+		auto exp = v0 ^ v1;
+		EXPECT_THAT(exp(0), Eq(0));
+		EXPECT_THAT(exp(1), Eq(0));
+		EXPECT_THAT(exp(2), Eq(1));
+	}
+
+	TEST(Matrix_Math_Test, solve)
+	{
+		mat3 A = get_identity<3, float>();
+
+		vec3 b{ 0,1,0 };
+		vec3 exp{ 0,1,0 };
+
+		auto act = b / A;
+
+		EXPECT_THAT(act(0), Eq(exp(0)));
+		EXPECT_THAT(act(1), Eq(exp(1)));
+		EXPECT_THAT(act(2), Eq(exp(2)));
+	}
+}
