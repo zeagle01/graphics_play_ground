34d3f5290d4812fd8081eca0c563218235ad41d4
zeagle <zeagle01@163.com>
2022-10-15 15:21:00 +0800

    create window

--- examples/soft_render_2/main.cpp
+++ examples/soft_render_2/main.cpp
@@ -1,8 +1,68 @@
 
 #include <iostream> 
 
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+void init_window()
+{
+
+
+	auto status = glfwInit();
+	if (status == GLFW_TRUE)
+	{
+		printf("glfw init succeed! \n");
+	}
+	else
+	{
+		printf("glfw init error! \n");
+	}
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(800, 600, "gl window", nullptr, nullptr);
+
+	if (window)
+	{
+		printf(" window %p is created\n", window);
+	}
+	else
+	{
+		printf(" window can't be created\n");
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glfwSwapInterval(1);
+
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+		printf("glad load failed\n");
+	}
+	else {
+
+		printf("glad load succend\n");
+	}
+	printf("gl version %s\n", glGetString(GL_VERSION));
+
+	while (!glfwWindowShouldClose(window))
+	{
+
+		glClearColor(0., 0.3, 0.5, 1.);//default color;
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+
+}
+
 int main()
 {
 	printf(" hello world");
+
+	init_window();
+
 	return 0;
 }
