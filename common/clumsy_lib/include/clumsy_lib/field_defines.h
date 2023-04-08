19557d6e6fef97472bddeca883cc96784f5efd29
zeagle <zeagle01@163.com>
2023-04-09 00:24:17 +0800

    set default value

--- common/clumsy_lib/include/clumsy_lib/field_defines.h
+++ common/clumsy_lib/include/clumsy_lib/field_defines.h
@@ -3,3 +3,4 @@
 #pragma once
 
 #define ADD_FIELD(name,t) struct name{ using type=t;}; name*  name##_var;
+#define ADDD_FIELD_WITH(name,t,...) struct name{ using type=t; __VA_ARGS__}; name*  name##_var;
--- unit_test/common/clumsy_lib_test/static_type_map_test.cpp
+++ unit_test/common/clumsy_lib_test/static_type_map_test.cpp
@@ -4,6 +4,7 @@
 #include "clumsy_lib/static_type_map.h"
 #include "clumsy_lib/field_defines.h"
 #include "clumsy_lib/class_reflection.h"
+#include "clumsy_lib/static_loop.h"
 
 using namespace testing;
 using namespace clumsy_lib;
@@ -41,4 +42,49 @@ TEST(Static_Type_Map_Test, get_B)
 	float act = m.get_ref<My_List::B>();
 
 	EXPECT_THAT(act, Eq(exp));
+}
+
+
+/////////////////////////////////////////
+
+template<typename T,T v>
+struct LiteralValue
+{
+	static constexpr auto apply()
+	{
+		return v;
+	}
+};
+
+
+struct Var_With_Defualt_Value
+{
+	ADDD_FIELD_WITH(A, int, ADD_FIELD(Get_Default, EVAL(LiteralValue<int,2>)))
+	ADDD_FIELD_WITH(B, int, ADD_FIELD(Get_Default, EVAL(LiteralValue<float,3.f>)))
+
+};
+
+struct Set_Defual_Value
+{
+	template<typename Var_Name,typename Type_Map>
+	static void apply(Type_Map& tm)
+	{
+		tm.get_ref<Var_Name>() = Var_Name::Get_Default::type::apply();
+	}
+
+};
+
+
+TEST(Static_Type_Map_Test, set_defualt_value)
+{
+	using default_value_vars = extract_member_type_list_t<Var_With_Defualt_Value>;
+	clumsy_lib::Static_Type_Map<default_value_vars> m;
+
+	For_Each_Type<default_value_vars>::apply<Set_Defual_Value>(m);
+
+	int actA = m.get_ref<Var_With_Defualt_Value::A>();
+	EXPECT_THAT(actA, Eq(2));
+
+	float actB = m.get_ref<Var_With_Defualt_Value::B>();
+	EXPECT_THAT(actB, Eq(3.f));
 }
