b6d81cb9a1c19ab37d3e4f61d69e6284a0012265
zeagle <zeagle01@163.com>
2023-08-26 14:38:03 +0800

    solver

--- common/sim_lib/simulator_imp.cpp
+++ common/sim_lib/simulator_imp.cpp
@@ -4,17 +4,31 @@ module;
 #include "clumsy_lib/static_type_map.h"
 #include "clumsy_lib/class_reflection.h"
 
-#include <algorithm>
-#include <cmath>
-#include <array>
+#include "matrix_math/matrix_math.h"
 
 
 module sim_lib : simulator_imp;
 
 import :sim_data;
+import :solver;
 
 namespace sim_lib
 {
+
+
+	template<typename T, int N>
+	static void convert(std::vector<std::array<T, N>>& out, const std::vector<matrix_math::mat<N, 1, T>>& in)
+	{
+		out.resize(in.size());
+		for (int i = 0; i < in.size(); i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				out[i][j] = in[i](j);
+			}
+		}
+
+	}
 
 	class simulator_imp
 	{
@@ -35,26 +49,16 @@ namespace sim_lib
 
 		void step()
 		{
-			auto& pos = m_datas.get_ref<sim_data::positions>();
-
-			float amp = 0.01f;
-			float freq = 1.0f;
-			float step = 0.01f;
-
-			int vNum = pos.size();
-			for (int i = 0; i < vNum; i++)
-			{
-				pos[i][0] += amp * std::sin(t * freq);
-			}
-
-			t += step;
+			m_solver.update_data(m_datas);
+			m_solver.solve();
+			convert(m_datas.get_ref<sim_data::positions>(), m_solver.get_result());
 		}
 
 	private:
 		using var_list = clumsy_lib::extract_member_type_list_t<sim_data>;
 		clumsy_lib::Static_Type_Map<var_list> m_datas;
 
-		float t = 0.f;
+		solver m_solver;
 
 	};
 }
---
+++ common/sim_lib/solver.cpp
@@ -0,0 +1,81 @@
+
+module;
+
+#include "clumsy_lib/static_type_map.h"
+#include "clumsy_lib/field_defines.h"
+#include "clumsy_lib/class_reflection.h"
+#include "matrix_math/matrix_math.h"
+
+#include <cmath>
+
+module sim_lib:solver;
+
+import :sim_data;
+
+
+namespace sim_lib
+{
+	//
+	using vec3 = matrix_math::mat<3, 1, float>;
+	using mat3 = matrix_math::mat<3, 3, float>;
+
+	template<typename T, int N>
+	static void convert(std::vector<matrix_math::mat<N, 1, T>>& out, const std::vector<std::array<T, N>>& in)
+	{
+		out.resize(in.size());
+		for (int i = 0; i < in.size(); i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				out[i](j) = in[i][j];
+			}
+		}
+
+	}
+
+
+	class solver
+	{
+	public:
+		template<typename data_holder>
+		void update_data(const data_holder& data)
+		{
+			convert(m_datas.get_ref<var::positions>(), data.get_ref<sim_data::positions>());
+		}
+
+		void solve()
+		{
+			float amp = 0.01f;
+			float freq = 1.0f;
+			float step = 0.01f;
+
+			auto& pos = m_datas.get_ref<var::positions>();
+			int vNum = pos.size();
+			for (int i = 0; i < vNum; i++)
+			{
+				pos[i](0) += amp * std::sin(t * freq);
+			}
+
+			t += step;
+
+		}
+
+		const std::vector<vec3>& get_result() const
+		{
+			return m_datas.get_ref<var::positions>();
+		}
+
+	private:
+
+		struct var
+		{
+			CE_ADD_FIELD(positions, std::vector<vec3>);
+
+		};
+		using var_list = clumsy_lib::extract_member_type_list_t<var>;
+		clumsy_lib::Static_Type_Map<var_list> m_datas;
+
+		float t = 0.f;
+	};
+
+}
