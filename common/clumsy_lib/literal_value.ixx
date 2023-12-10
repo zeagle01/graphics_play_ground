9c422c1a69337c674a33809f8e42c99ad268235b
zeagle <zeagle01@163.com>
2023-12-10 15:18:15 +0800

    literal value and strings

--- common/clumsy_lib/literal_value.ixx
+++ common/clumsy_lib/literal_value.ixx
@@ -1,5 +1,7 @@
 module;
 
+#include <array>
+#include <string_view>
 
 
 export module clumsy_lib : literal_value;
@@ -12,26 +14,33 @@ namespace clumsy_lib
 	template<auto v>
 	struct literal_value
 	{
+		static constexpr decltype(v) value = v;
 
-		template<typename T>  
-		static void assign(T& t)
-		{
-			t = v;
-		}
+	};
 
+	template<int N>
+	struct literal_string_storage
+	{
+		constexpr literal_string_storage(const char (&data_ref)[N]) { std::copy_n(data_ref, N, data.data()); }
+		std::array<char,N> data;
 	};
 
 	export 
-		template<typename T, T v >
-	struct literal_value<v>
+	template<literal_string_storage str>
+	struct literal_string 
 	{
-
-		static void assign(T& t)
-		{
-			t = v;
-		}
-
+		static constexpr std::string_view value = str.data.data();
 	};
+
+
+	export 
+	template<typename T, T v >
+	struct literal_custom
+	{
+		static constexpr decltype(v) value = v;
+	};
+
+
 }
 
 
--- unit_test/common/clumsy_lib_test/literal_value_test.cpp
+++ unit_test/common/clumsy_lib_test/literal_value_test.cpp
@@ -1,6 +1,7 @@
 
 #include "gmock/gmock.h"
 #include <vector>
+#include <array>
 
 
 import clumsy_lib;
@@ -9,12 +10,46 @@ import clumsy_lib;
 using namespace testing;
 using namespace clumsy_lib;
 
-
-
-TEST(Literal_Value_Test,assign)
+namespace literal_value_test
 {
-	int a;
-	literal_value<3> ::assign(a);
-	EXPECT_THAT(a, Eq(3));
+
+
+	enum class my_enum
+	{
+		A,
+		B,
+		C
+	};
+
+	TEST(Literal_Value_Test, assign_int)
+	{
+		int value = literal_value<3> ::value;
+		EXPECT_THAT(value, Eq(3));
+	}
+
+	TEST(Literal_Value_Test, assign_int_2)
+	{
+		using int2 = std::array<int, 2>;
+		auto value = literal_custom < int2, int2{ 1,2 } > ::value;
+		EXPECT_THAT(value, Eq(int2{ 1,2 }));
+	}
+
+	TEST(Literal_Value_Test, assign_enum)
+	{
+		my_enum value = literal_value<my_enum::A> ::value;
+		EXPECT_THAT(value, Eq(my_enum::A));
+	}
+
+
+
+	TEST(Literal_Value_Test, assign_string)
+	{
+		std::string value = literal_string<"ABC"> ::value.data();
+		EXPECT_THAT(value, Eq("ABC"));
+
+	}
+
 }
+
+
 
